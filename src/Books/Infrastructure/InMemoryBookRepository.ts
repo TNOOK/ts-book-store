38d3cf86eace78c54d51d8fee00b711029565856
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-03-19 18:18:33 +0100

    Improved InMemoryBookRepository

--- src/Books/Infrastructure/InMemoryBookRepository.ts
+++ src/Books/Infrastructure/InMemoryBookRepository.ts
@@ -6,26 +6,26 @@ import {BookName} from "../Domain/BookName";
 import {BookLength} from "../Domain/BookLength";
 
 export class InMemoryBookRepository implements BookRepository {
+    private books: Book[];
+    constructor() {
+        this.books = [
+            new Book(new BookId(v4()), BookName.fromString('La biblia'), new BookLength(200)),
+            new Book(new BookId(v4()), BookName.fromString('El Quijote'), new BookLength(230)),
+            new Book(new BookId(v4()), BookName.fromString('Las Reliquias de la muerte'), new BookLength(300)),
+            new Book(new BookId(v4()), BookName.fromString('Node for dummies'), new BookLength(800))
+        ]
+    }
+
     find(id: BookId): Book {
-        return new Book(
-            new BookId(v4()),
-            BookName.fromString('Don Quijote'),
-            new BookLength(20)
-        );
+        const book =  this.books.find(book => book.id === id);
+        if (typeof book === 'undefined') {
+            new Error('There is not any book with that id');
+        }
+        // @ts-ignore
+        return book;
     }
 
     findAll(): Book[] {
-        return [
-            new Book(
-                new BookId(v4()),
-                BookName.fromString('Don Quijote'),
-                new BookLength(20)
-            ),
-            new Book(
-                new BookId(v4()),
-                BookName.fromString('La Biblia'),
-                new BookLength(500)
-            )
-        ];
+        return this.books;
     }
 }
