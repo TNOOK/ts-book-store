af20ebc0535f704ed6721c31eb9bbde19667cba3
Diego <garciasdos@gmail.com>
2020-03-20 20:40:02 +0100

    Add GetBookControllerResponse

--- src/Books/Application/BookFinder.ts
+++ src/Books/Application/BookFinder.ts
@@ -1,12 +1,18 @@
 import {Book} from "../Domain/Book";
 import {BookRepository} from "../Domain/BookRepository";
 import {BookId} from "../Domain/BookId";
+import {GetBookControllerResponse} from "../Domain/GetBookControllerResponse";
 
 export class BookFinder {
     constructor(private repository: BookRepository) {}
 
-    async run(id: BookId): Promise<Book> {
-        return this.repository.find(id);
+    async run(id: BookId): Promise<GetBookControllerResponse> {
+        const book = this.repository.find(id);
+        return new GetBookControllerResponse(
+            book.id.uuid,
+            book.name.value,
+            book.length.value
+        );
     }
 
 }
