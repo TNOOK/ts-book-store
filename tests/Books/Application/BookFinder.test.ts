cf308a90e6dbb8abcafef8ac5ff554c7f1636273
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-03-21 17:20:45 +0100

    Fixed BookFinder test
    Removed unused dependencies in BookId
    Changed BookFinder to take string id when GetBookControllerResponse is created
    Refactored BookFinder test

--- tests/Books/Application/BookFinder.test.ts
+++ tests/Books/Application/BookFinder.test.ts
@@ -7,21 +7,21 @@ import {BookLength} from "../../../src/Books/Domain/BookLength";
 
 describe('Book Finder', () => {
     it('should create a valid course', async () => {
-        const find = jest.fn();
+        const mockId = new BookId('4f608969-ec4c-4e66-bf70-3a98fe7d7ca4');
+        const jestFind = jest.fn();
+        const find =  (bookId: BookId) => {
+            jestFind(bookId);
+            const id = bookId;
+            const name = BookName.fromString('some-name');
+            const length = new BookLength(15);
+            return new Book(id, name, length);
+        };
         const repository: BookRepository = {
             find,
             findAll: jest.fn()
         };
-
         const findBook = new BookFinder(repository);
-
-        const id = new BookId('4f608969-ec4c-4e66-bf70-3a98fe7d7ca4');
-        const name = BookName.fromString('some-name');
-        const length = new BookLength(15);
-
-        const book = new Book(id, name, length);
-
-        await findBook.run(id);
-        expect(find).toHaveBeenCalledWith(id);
+        await findBook.run(mockId);
+        expect(jestFind).toHaveBeenCalledWith(mockId);
     });
 });
