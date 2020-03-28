1d3c2290e731d600468eb3610f15ab55f0d2b764
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-03-28 16:03:45 +0100

    Create delete controller to remove books
    Add new application service to manage delete flow
    Add new dependency injection to use it

--- app/backend/config/routes/books.route.ts
+++ app/backend/config/routes/books.route.ts
@@ -3,12 +3,15 @@ import GetBooksController from "../../controllers/GetBooksController";
 import GetBookController from "../../controllers/GetBookController";
 import PostBookController from "../../controllers/PostBookController";
 import container from "../dependency-injection";
+import DeleteBookController from "../../controllers/DeleteBookController";
 
 export const register = (app: Express) => {
   const getBooksController: GetBooksController = container.get('BookStore.GetBooksController'),
       getBookController: GetBookController = container.get('BookStore.GetBookController'),
-      postBookController: PostBookController = container.get('BookStore.PostBookController');
+      postBookController: PostBookController = container.get('BookStore.PostBookController'),
+      deleteBookControllerL: DeleteBookController = container.get('BookStore.DeleteBookController');
   app.get('/books/', getBooksController.run.bind(getBooksController));
   app.get('/books/:id', getBookController.run.bind(getBookController));
   app.post('/books/', postBookController.run.bind(postBookController));
+  app.delete('/books/:id', deleteBookControllerL.run.bind(deleteBookControllerL));
 };
