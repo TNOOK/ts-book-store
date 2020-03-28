9518291b18212a386b92e18c6610896287ea1cb6
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-03-28 20:24:41 +0100

    Validate arguments in value object
    JS problems, for any reason, if i send a request to create a new book without name or length, it will be created without any error.
    There is a very similar problem if you send, for example, 7 instead a string name as a book atribute. It will be created anyway

--- src/Books/Domain/BookName.ts
+++ src/Books/Domain/BookName.ts
@@ -1,12 +1,22 @@
+import validate from "uuid-validate";
+import {InvalidArgumentError} from "../../Shared/Domain/ValueObject/InvalidArgumentException";
+
 export class BookName {
     private readonly _value: string;
 
     private constructor(value: string) {
+        this.ensureIsValidValue(value);
         this._value = value;
     }
 
     public static fromString(value: string): BookName {
         return new BookName(value);
+    }
+
+    private ensureIsValidValue(value: string): void {
+        if (typeof value !== 'string') {
+            throw new InvalidArgumentError(`<${this.constructor.name}> does not allow the value <${value}>`);
+        }
     }
 
     get value(): string {
